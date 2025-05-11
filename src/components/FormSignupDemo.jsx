7cf5559f2710e8ed742555a3bbe286c8bec00d90
fraulueneburg <github@fraulueneburg.de>
2025-05-11 23:51:47 +0200

    feat: added loading animation to no-signup signup button

--- src/components/FormSignupDemo.jsx
+++ src/components/FormSignupDemo.jsx
@@ -1,31 +1,37 @@
 import React from 'react'
 import axios from 'axios'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../context/auth.context'
 import { API_URL } from '../config'
 
 import currenciesArr from '../data/currencies.json'
+import Spinner from './Spinner'
 
 export default function FormSignupDemo() {
 	const { setToken, setIsLoggedIn } = useContext(AuthContext)
+	const [isLoading, setIsLoading] = useState(false)
 	const navigate = useNavigate()
 	const indexDefaultCurrency = currenciesArr.findIndex((elem) => elem.symbol === '€')
 
 	const handleRecruiterSubmit = async (e) => {
+		setIsLoading((prev) => !prev)
+		setIsLoading(true)
 		e.preventDefault()
 		try {
 			const { data } = await axios.post(`${API_URL}/auth/one-click-signup`, {
 				currency: currenciesArr[indexDefaultCurrency],
 			})
-
 			const actualToken = data.authToken
 			setToken(actualToken)
 			setIsLoggedIn(true)
+
+			setIsLoading(false)
 			navigate('/budget')
 		} catch (err) {
 			console.log('Error during no-signup signup:', err)
 		}
+		setIsLoading(false)
 	}
 
 	return (
@@ -37,7 +43,8 @@ export default function FormSignupDemo() {
 				<li>account self-deletes after 24h</li>
 			</ul>
 			<button type="button" onClick={handleRecruiterSubmit}>
-				One-Click Demo Signup
+				<span>One-Click Demo Signup</span>
+				{isLoading ? <Spinner /> : null}
 			</button>
 		</>
 	)
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,7 @@
+import './../css/spinner.scss'
+
+const Spinner = () => {
+	return <div className="spinner" aria-label="loading" role="status"></div>
+}
+
+export default Spinner
